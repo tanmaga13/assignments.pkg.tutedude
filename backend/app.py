13f3aa718bcbb8e96e30ed8e44d6b4dd5ef9f3de
Tanmay Agarwal <ubuntu@ip-172-31-12-186.ap-south-2.compute.internal>
2026-02-16 16:35:33 +0000

    Docker Assignment running

--- backend/app.py
+++ backend/app.py
@@ -43,5 +43,5 @@ def submit():
     collection.insert_one(form_data)
     return "Data Submitted Successfully"
 
-if __name__ == "__main__ ":
+if __name__ == "__main__":
     app.run(port=8000, host = '0.0.0.0', debug=True)
